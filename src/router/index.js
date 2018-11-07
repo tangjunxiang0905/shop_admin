ee632a9a08aedd2b4cc4ff1768d9a91755cf757b
tangjunxiang0905 <1976259749@qq.com>
2018-11-07 22:59:52 +0800

    项目初始化提交

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 导入登录组件
+import Login from '@/components/Login.vue'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
